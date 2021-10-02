d0d2152b7a4860ba0642765c014a09b34d70c0b9
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2021-10-02 07:39:28 +0530

    feat: add test-wise html output

--- html-reporter.js
+++ html-reporter.js
@@ -2,20 +2,26 @@
  * html report generator
  */
 
-let router = require('./route.js');
+let router = require("./route.js");
 let config = require(router.config);
-let d = (new Date).toISOString().replace(/:|z|t/gi, '-').replace(/\..+/, '').trim();
-let HtmlReporter = require('nightwatch-html-reporter');
-/* Same options as when using the built in nightwatch reporter option */
-let reporter = new HtmlReporter({
-    openBrowser: true,
-    reportsDirectory: __dirname + '/html-reports/',
-    reportFilename: config.siteName + "-" + d + ".html",
-    themeName: 'default'// cover, compact, compact-gray, default-gray, outlook
-});
+let d = new Date()
+  .toISOString()
+  .replace(/:|z|t/gi, "-")
+  .replace(/\..+/, "")
+  .trim();
+let HtmlReporter = require("nightwatch-html-reporter");
 
 module.exports = {
-    write: function (results, options, done) {
-        reporter.fn(results, done);
+  write: function (results, options, done) {
+    for (const test_case in results.modules) {
+      /* Same options as when using the built in nightwatch reporter option */
+      let reporter = new HtmlReporter({
+        openBrowser: true,
+        reportsDirectory: __dirname + "/html-reports/",
+        reportFilename: `${config.siteName}-${test_case}-${d}.html`,
+        themeName: "default", // cover, compact, compact-gray, default-gray, outlook
+      });
+      reporter.fn(results.modules[test_case], done);
     }
+  },
 };
