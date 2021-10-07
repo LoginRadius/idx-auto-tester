85cf44260fec34d9adc8dba8af87039953000e54
Santosh Bhandari <contact@santoshb.com.np>
2021-10-07 22:55:31 +0545

    refactor: Replace request with node-fetch
    Replaced the deprecated request module with node-fetch module and updated the code accordingly

--- helpers/mail7.js
+++ helpers/mail7.js
@@ -1,92 +1,84 @@
-let router = require('../route.js');
-let config = require(router.config);
-var request = require('request'),
-    defaultHost = 'https://api.mail7.io',
-    text_body;
+const router = require('../route.js');
+const config = require(router.config);
+const fetch = require('node-fetch');
 
+const { URL } = require('url');
+
+const DEFAULT_HOST = 'https://api.mail7.io';
+
+/**
+ * @param {String} e: email in form of email(zyx@mail7.io or zyx), you want get data from...
+ * @param {function} cb: callback function to handle response
+ */
 module.exports = function (e, cb) {
-
-    /**
-     * @param {String} e: email in form of email(zyx@mail7.io or zyx), you want get data from...
-     *
-     * @param {function} cb: callback function to handle response
-     */
-
-    let domain = e.substr(e.indexOf('@'))
+    let domain = e.substr(e.indexOf('@'));
     e = e.indexOf('@') > -1 ? e.replace(domain, '') : e;
 
-    request(defaultHost + '/inbox?apikey=' + config.mail7ApiKey + '&apisecret=' + config.mail7ApiSecret + '&to=' + e + "&domain=" + domain.replace('@', ''), function (err, res) {
+    const fetchUrl = new URL(`${DEFAULT_HOST}/inbox`);
 
-        var output = {};
+    fetchUrl.searchParams.set('apikey', config.mail7ApiKey);
+    fetchUrl.searchParams.set('apisecret', config.mail7ApiSecret);
+    fetchUrl.searchParams.set('to', e);
+    fetchUrl.searchParams.set('domain', domain.replace('@', ''));
 
-        if (!err) {
-
-            var body = JSON.parse(res.body);
-
-            var subjects = [];
-
+    let output = {};
+    fetch(fetchUrl.href)
+        .then((res) => res.json())
+        .then((body) => {
+            const subjects = [];
             if (body.status === 'success') {
-
                 if (body.data.length > 0) {
-
-                    for (var i in body.data) {
+                    for (const i in body.data) {
                         subjects.push(body.data[i].mail_source.subject);
                     }
-
-                    var html = body.data[0].mail_source.html;
+                    const html = body.data[0].mail_source.html;
 
                     output.status = 'success';
-                    output.subjects = subjects; //array of all subjects
+                    output.subjects = subjects; // array of all subjects
                     output.state = 0;
-                    output.text = html.replace(/<\/?[^>]+(>|$)/g, " "); // simple text body
-                    text_body = output.text;
+                    output.text = html.replace(/<\/?[^>]+(>|$)/g, ' '); // simple text body
 
                     /**
                      * Extract token from mail body
-                     *
                      * returned tokens: verification token, reset token, oneclicksign, delete token etc..
                      */
-
-                    if (html.indexOf("vtoken=") > -1) {
-
-                        var indexValue = html.indexOf("vtoken=");
-                        var remainingText = html.substr(indexValue + 7, 100);
+                    if (html.indexOf('vtoken=') > -1) {
+                        const indexValue = html.indexOf('vtoken=');
+                        const remainingText = html.substr(indexValue + 7, 100);
                         //find gap in string
                         //var gap = remainingText.indexOf(' ');
-                        var vtoken = remainingText.substr(0, 32);
-                        var emailOTP = remainingText.substr(0, 6);
+                        const vtoken = remainingText.substr(0, 32);
+                        const emailOTP = remainingText.substr(0, 6);
 
                         if (typeof vtoken !== 'undefined' && vtoken) {
                             output.vtoken = vtoken;
                             output.emailOTP = emailOTP;
-                            if (vtoken.indexOf(' ') > -1 || vtoken.indexOf('<br/>') > -1)
-                                output.vtoken = null;
-                            else
-                                output.emailOTP = null;
+                            if (vtoken.indexOf(' ') > -1 || vtoken.indexOf('<br/>') > -1) output.vtoken = null;
+                            else output.emailOTP = null;
                         } else {
                             output.status = 'success';
                             output.state = 0;
-                            output.vtoken = "Unable to locate vtoken in recent mail body...";
+                            output.vtoken = 'Unable to locate vtoken in recent mail body...';
                         }
                     }
                 } else {
                     output.status = 'error';
                     output.state = -1;
-                    output.message = "mail_source is not found for this username...";
-                    output.body = res.body;
+                    output.message = 'mail_source is not found for this username...';
+                    output.body = body;
                 }
             } else {
                 output.status = 'error';
                 output.state = -1;
                 output.message = body.message;
             }
-
-        } else {
+        })
+        .catch((err) => {
             output.status = 'error';
             output.state = -1;
             output.body = err;
-        }
-        cb(output)
-    })
-
+        })
+        .finally(() => {
+            cb(output);
+        });
 };
